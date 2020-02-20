294cb0960171ed900288ec4153993cce5bdd55f8
TanmaySolanki <tn945593@dal.ca>
2020-02-20 12:51:36 -0500

    task0 done

--- fizzbuzztask.py
+++ fizzbuzztask.py
@@ -5,14 +5,17 @@ Created on Thu Feb 20 12:40:31 2020
 @author: Tj
 """
 
-listonum=[]
+
 for i in range(1,101):
-    if i%3==0:
+    if i%3==0 and i%5==0:
+        print('fizz buzz')
+        
+    elif i%3==0:
         print('fizz')
+        
     elif i%5==0:
         print('buzz')
-    elif i%3==0 and i%5==0:
-        print('fizz buzz')
+        
     else:
         print(i)
 
