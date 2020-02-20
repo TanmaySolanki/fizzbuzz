32ac8099da8e876fff903003d0efff7d58a256c2
TanmaySolanki <tn945593@dal.ca>
2020-02-20 12:48:51 -0500

    in process

---
+++ fizzbuzztask.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 12:40:31 2020
+
+@author: Tj
+"""
+
+listonum=[]
+for i in range(1,101):
+    if i%3==0:
+        print('fizz')
+    elif i%5==0:
+        print('buzz')
+    elif i%3==0 and i%5==0:
+        print('fizz buzz')
+    else:
+        print(i)
+
+        
